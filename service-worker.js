0d9317e30762c1ab7f6fb86192eddfc3cd84db36
Anna <garsinaanna32778@gmail.com>
2021-03-15 17:06:26 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.dd2d56e5be17d37ea0104f2a9944b23b.js"
 );
 
 self.addEventListener('message', (event) => {
